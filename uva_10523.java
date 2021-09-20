2d254c64c4e706b668b561e0c08abedb73c91a8b
Shad-Alam <shad.19bd@gmail.com>
2021-09-20 13:45:59 +0600

    very easy

--- uva_10523.java
+++ uva_10523.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
-		
 		BigInteger sum,n1;
 		
 		while(input.hasNext()){
