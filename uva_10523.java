98cb09bfdd61a06d6d059427ac1cd9f4d8922b97
Shad-Alam <shad.19bd@gmail.com>
2021-09-20 13:27:33 +0600

    Very Easy git add .!

---
+++ uva_10523.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		BigInteger sum,n1;
+		
+		while(input.hasNext()){
+			int N = input.nextInt();
+			int A = input.nextInt();
+						
+			sum = new BigInteger("0");
+			n1 = BigInteger.valueOf(A);
+			for(int i=1; i<=N; i++){
+				sum = sum.add(BigInteger.valueOf(i).multiply(n1.pow(i)));
+			}System.out.println(sum);
+		}
+	}
+}
