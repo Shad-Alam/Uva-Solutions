aed8e0b1f455377f9eb0dd14515a71f14b0cc703
Shad-Alam <shad.19bd@gmail.com>
2021-12-23 22:16:13 +0600

    Uva 11044

---
+++ uva_11044.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){					
+	int t; cin >> t;
+	
+	while(t--){
+		int n, m; cin >> n >> m;
+		int s1 = m/3, s2 = n/3;
+		int s = s1*s2;
+		cout << s << endl;
+	}
+	
+	return 0;
+}
